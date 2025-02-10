e238bad5dd523739f8b13222ee21455cfafc25f5
shubha-shree-r <shubhashree1805@gmail.com>
2025-02-10 16:23:32 +0530

    linkedlist

---
+++ src/arraysAndCollectionsFramework/LinkedLists.java
@@ -0,0 +1,27 @@
+package arraysAndCollectionsFramework;
+
+import java.util.LinkedList;
+
+public class LinkedLists {
+
+    public static void main(String[] args) {
+        LinkedList<String> links = new LinkedList<>();
+        links.add("Apple");
+        links.addFirst("Banana");
+        links.addLast("Orange");
+
+        System.out.println(links);
+        System.out.println(links.get(2));
+        System.out.println(links.getFirst());
+        System.out.println(links.remove(1));
+        System.out.println(links);
+        System.out.println(links.add("Apple"));
+        System.out.println(links);
+
+        System.out.println("   ");
+        for (int i = 0; i < links.size(); i++) {
+            String fruit = links.get(i);
+            System.out.println(fruit);
+        }
+    }
+}
