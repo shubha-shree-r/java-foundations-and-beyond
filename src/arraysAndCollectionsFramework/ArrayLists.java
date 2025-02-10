ca1ae72d1d85b44891f0cff6e523b0667c2d02bd
shubha-shree-r <shubhashree1805@gmail.com>
2025-02-10 16:11:25 +0530

    arraylist

---
+++ src/arraysAndCollectionsFramework/ArrayLists.java
@@ -0,0 +1,19 @@
+package arraysAndCollectionsFramework;
+
+import java.util.ArrayList;
+
+public class ArrayLists {
+
+    public static void main(String[] args) {
+        ArrayList<String> fruits = new ArrayList<>();
+        fruits.add("Apple");
+        fruits.add("Banana");
+        fruits.add("Orange");
+
+        fruits.add(1,"Grapes");
+
+        System.out.println(fruits);
+    }
+
+
+}
